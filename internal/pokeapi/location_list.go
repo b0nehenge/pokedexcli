02fb7d22a8b7bd155683e0beccae0b82febacf83
b0nehenge <metallica99mm@gmail.com>
2026-02-15 08:23:32 -0800

    added ability to catch and inspect pokemon

--- internal/pokeapi/location_list.go
+++ internal/pokeapi/location_list.go
@@ -7,33 +7,75 @@ import (
 )
 
 // ListLocations -
-func (c *Client) ListLocations(pageURL *string) (RespShallowLocations, error) {
+func (c *Client) ListLocations(pageURL *string) (LocationList, error) {
 	url := baseURL + "/location-area"
 	if pageURL != nil {
 		url = *pageURL
 	}
 
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		return RespShallowLocations{}, err
+	if val, ok := c.cache.Get(url); ok {
+		locationsResp := LocationList{}
+		err := json.Unmarshal(val, &locationsResp)
+		if err != nil {
+			return LocationList{}, err
+		}
+
+		return locationsResp, nil
 	}
 
-	resp, err := c.httpClient.Do(req)
+	resp, err := http.Get(url)
+
 	if err != nil {
-		return RespShallowLocations{}, err
+		return LocationList{}, err
 	}
 	defer resp.Body.Close()
 
 	dat, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return RespShallowLocations{}, err
+		return LocationList{}, err
 	}
 
-	locationsResp := RespShallowLocations{}
+	locationsResp := LocationList{}
 	err = json.Unmarshal(dat, &locationsResp)
 	if err != nil {
-		return RespShallowLocations{}, err
+		return LocationList{}, err
 	}
 
+	c.cache.Add(url, dat)
+	return locationsResp, nil
+}
+
+func (c *Client) ExploreLocation(location string) (Location, error) {
+	url := baseURL + "/location-area/" + location
+
+	if val, ok := c.cache.Get(url); ok {
+		locationsResp := Location{}
+		err := json.Unmarshal(val, &locationsResp)
+		if err != nil {
+			return Location{}, err
+		}
+
+		return locationsResp, nil
+	}
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return Location{}, err
+	}
+
+	defer resp.Body.Close()
+
+	dat, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return Location{}, err
+	}
+
+	locationsResp := Location{}
+	err = json.Unmarshal(dat, &locationsResp)
+	if err != nil {
+		return Location{}, err
+	}
+
+	c.cache.Add(url, dat)
 	return locationsResp, nil
 }
